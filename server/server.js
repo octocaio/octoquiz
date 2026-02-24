de17929bd4ba365e167d8d1ab41dfe9783494606
caio queiroz <caiocqueiroz@github.com>
2026-02-24 01:39:19 -0300

    octoquiz

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173',
     methods: ['GET', 'POST'],
   },
 });
@@ -210,7 +210,7 @@ function endQuestion(pin, room) {
   });
 }
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 httpServer.listen(PORT, () => {
   console.log(`OctoQuiz server running on port ${PORT}`);
 });
